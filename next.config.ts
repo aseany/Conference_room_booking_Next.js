bb6914c8c507daa628a6c752bc701c371b7d5a12
Tomohisa Kitamura） <tomohisa520519@gmail.com>
2026-09-02 07:08:42 +0900

    20260902 tomohisa

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
+// Next.jsアプリ全体のビルド・実行時の挙動を設定するファイルです。プロジェクトのルートに1つ置かれ、NextConfig 型のオブジェクトをデフォルトエクスポートします。
 import path from "path";
 import type { NextConfig } from "next";
 
+// NextConfig は、Next.js が提供する TypeScript の型定義
 const nextConfig: NextConfig = {
+  // Turbopack開発元vercelのバンドラーに
   turbopack: {
     root: path.join(__dirname),
   },
+  // ↓Next.js 16 系の新機能で、next dev 実行時に AI コーディングエージェント(Claude Code など)を検出すると、自動的に AGENTS.md や CLAUDE.md 。デフォルトは true。
   agentRules: false,
 };
 
